0fe9e55cdc7ca88f2b74491cf69d1cf16f374406
yaoguipingboy <45711354+yaoguipingboy@users.noreply.github.com>
2018-12-10 17:03:11 +0800

    Update CloudSms.sql

--- CloudSms.sql
+++ CloudSms.sql
@@ -16,7 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 -- comment modified by hero on 20181210 
-
+-- modified on master branch on 20181210
 --
 -- 数据库: `sms`
 --
